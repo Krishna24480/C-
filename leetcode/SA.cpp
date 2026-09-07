a44005bcb52f9d51e9e83361301311141308629d
Krishna24480 <krajawat244@gmail.com>
2026-09-07 20:14:39 +0530

    C++

---
+++ leetcode/SA.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void swapAlternate(int arr[], int size)
+{
+    for (int i = 0; i < size; i += 2)
+    {
+        if (i + 1 < size)
+        {
+            swap(arr[i], arr[i + 1]);
+        }
+    }
+}
+
+void printArray(const int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original Array: ";
+    printArray(arr, size);
+
+    swapAlternate(arr, size);
+
+    cout << "Swapped Array:  ";
+    printArray(arr, size);
+
+    return 0;
+}
