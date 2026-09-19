18c3318d2ed69dca7e8582bd335efaea654c6282
Krishna24480 <krajawat244@gmail.com>
2026-09-19 10:25:49 +0530

    C++

--- lectures/Lecture47/DARLILL.cpp
+++ lectures/Lecture47/DARLILL.cpp
@@ -134,115 +134,115 @@ int BigningOfLoop(Node *head)
 
     return slow->data;
 }
-    // Detect and Remove Loop
-    void removeLoop(Node * head)
+// Detect and Remove Loop
+void removeLoop(Node *head)
+{
+    if (head == NULL)
     {
-        if (head == NULL)
-        {
-            return ;
-        }
-
-        Node *slow = head;
-        Node *fast = head;
-
-        // Step 1: Detect loop
-        while (slow != NULL && fast != NULL && fast->next != NULL)
-        {
-            slow = slow->next;
-            fast = fast->next->next;
-
-            if (slow == fast)
-            {
-                break;
-            }
-        }
-
-        // No loop
-        if (slow != fast)
-        {
-            return;
-        }
-
-        // Step 2: Find starting point of loop
-        slow = head;
-
-        while (slow != fast)
-        {
-            slow = slow->next;
-            fast = fast->next;
-        }
-
-        // Step 3: Find last node of loop
-        Node *temp = fast;
-
-        while (temp->next != slow)
-        {
-            temp = temp->next;
-        }
-
-        // Remove loop
-        temp->next = NULL;
+        return;
     }
 
-    // Print Linked List
-    void print(Node * head)
+    Node *slow = head;
+    Node *fast = head;
+
+    // Step 1: Detect loop
+    while (slow != NULL && fast != NULL && fast->next != NULL)
     {
-        Node *temp = head;
+        slow = slow->next;
+        fast = fast->next->next;
 
-        while (temp != NULL)
+        if (slow == fast)
         {
-            cout << temp->data << " -> ";
-            temp = temp->next;
+            break;
         }
-
-        cout << "NULL" << endl;
     }
 
-    // Main Function
-    int main()
+    // No loop
+    if (slow != fast)
     {
-        Node *head = NULL;
-
-        insertAtTail(head, 10);
-        insertAtTail(head, 20);
-        insertAtTail(head, 30);
-        insertAtTail(head, 40);
-        insertAtTail(head, 50);
-
-        // Create loop:
-        // 50 -> 30
-        createLoop(head, 3);
-
-        // Check loop
-        if (detectLoop(head))
-        {
-            cout << "Loop is present" << endl;
-        }
-        else
-        {
-            cout << "Loop is not present" << endl;
-        }
-
-      int Bigning = BigningOfLoop(head);
-
-      cout << "Bigning Of Loop Is: " << Bigning << endl;
-
-      // Remove loop
-      removeLoop(head);
-
-      // Check again
-      if (detectLoop(head))
-      {
-          cout << "Loop is present" << endl;
-      }
-        else
-        {
-            cout << "Loop is removed" << endl;
-        }
-
-        // Print after removing loop
-        cout << "Linked List:" << endl;
-        print(head);
-
-        return 0;
+        return;
     }
+
+    // Step 2: Find starting point of loop
+    slow = head;
+
+    while (slow != fast)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    // Step 3: Find last node of loop
+    Node *temp = fast;
+
+    while (temp->next != slow)
+    {
+        temp = temp->next;
+    }
+
+    // Remove loop
+    temp->next = NULL;
+}
+
+// Print Linked List
+void print(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+
+    cout << "NULL" << endl;
+}
+
+// Main Function
+int main()
+{
+    Node *head = NULL;
+
+    insertAtTail(head, 10);
+    insertAtTail(head, 20);
+    insertAtTail(head, 30);
+    insertAtTail(head, 40);
+    insertAtTail(head, 50);
+
+    // Create loop:
+    // 50 -> 30
+    createLoop(head, 3);
+
+    // Check loop
+    if (detectLoop(head))
+    {
+        cout << "Loop is present" << endl;
+    }
+    else
+    {
+        cout << "Loop is not present" << endl;
+    }
+
+    int Bigning = BigningOfLoop(head);
+
+    cout << "Bigning Of Loop Is: " << Bigning << endl;
+
+    // Remove loop
+    removeLoop(head);
+
+    // Check again
+    if (detectLoop(head))
+    {
+        cout << "Loop is present" << endl;
+    }
+    else
+    {
+        cout << "Loop is removed" << endl;
+    }
+
+    // Print after removing loop
+    cout << "Linked List:" << endl;
+    print(head);
+
+    return 0;
+}
